03e71c27a9673a9f29fd5ed6c0e7a533abcbb554
태준모 <sunjm135@naver.com>
2023-09-13 10:01:27 +0900

    :bulb: Comment: CourseService 주석 추가

--- backend/src/main/java/com/capsule/youkids/course/service/CourseService.java
+++ backend/src/main/java/com/capsule/youkids/course/service/CourseService.java
@@ -9,11 +9,15 @@ import java.util.UUID;
 
 public interface CourseService {
 
+    // 코스 저장
     void save(CourseRegistRequestDto courseRegistRequestDto);
 
+    // 유저의 코스 불러오기
     List<CourseResponseDto> getCourseIdsByUserId(UUID userID);
 
+    // 코스 수정
     void update(CourseUpdateRequestDto courseUpdateRequestDto);
 
+    // 코스 삭제
     void delete(CourseDeleteDto courseDeleteDto);
 }

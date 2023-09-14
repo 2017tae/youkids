faa981f36a7a306f7b155eebf33b40f07bd5d552
김재석 <jaeseki@gmail.com>
2023-09-14 16:23:37 +0900

    :sparkles: Feat: 캡슐 디테일 페이지 캐러셀 구현

--- frontend/lib/src/screens/capsule/capsule_detail_screen.dart
+++ frontend/lib/src/screens/capsule/capsule_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class CapsuleDetailScreen extends StatelessWidget {
+class CapsuleDetailScreen extends StatefulWidget {
   final String place, date, imgUrl, content;
   // place: 장소, date: 날짜, imgUrl: 사진 주소, content: 캡슐 내용
   const CapsuleDetailScreen({
@@ -12,7 +13,29 @@ class CapsuleDetailScreen extends StatelessWidget {
   });
 
   @override
+  State<CapsuleDetailScreen> createState() => _CapsuleDetailScreenState();
+}
+
+class _CapsuleDetailScreenState extends State<CapsuleDetailScreen> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+  @override
   Widget build(BuildContext context) {
+    final List<String> images = [
+      'https://images.unsplash.com/photo-1586882829491-b81178aa622e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
+      'https://images.unsplash.com/photo-1586871608370-4adee64d1794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2862&q=80',
+      'https://images.unsplash.com/photo-1586901533048-0e856dff2c0d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+      'https://images.unsplash.com/photo-1586902279476-3244d8d18285?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80',
+      'https://images.unsplash.com/photo-1586943101559-4cdcf86a6f87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1556&q=80',
+      'https://images.unsplash.com/photo-1586951144438-26d4e072b891?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+      'https://images.unsplash.com/photo-1586953983027-d7508a64f4bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
+    ];
+    final List<Widget> imageSliders = images
+        .map(
+          (item) => Image.network(item,
+              fit: BoxFit.cover, width: MediaQuery.of(context).size.width),
+        )
+        .toList();
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -54,10 +77,41 @@ class CapsuleDetailScreen extends StatelessWidget {
               ],
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
-            color: const Color(0xffF5EEEC),
+          CarouselSlider(
+            items: imageSliders,
+            carouselController: _controller,
+            options: CarouselOptions(
+              height: MediaQuery.of(context).size.width,
+              viewportFraction: 1.0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, // 동그라미들을 가로로 중앙에 정렬
+            children: [
+              ...images.asMap().entries.map(
+                (entry) {
+                  return GestureDetector(
+                    onTap: () => _controller.animateToPage(entry.key),
+                    child: Container(
+                      width: 12.0,
+                      height: 15.0,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 4.0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: const Color(0xffFF7E76)
+                            .withOpacity(_current == entry.key ? 1 : 0.3),
+                      ),
+                    ),
+                  );
+                },
+              ).toList(),
+            ],
           ),
         ],
       ),

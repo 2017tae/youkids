d3aa5fb6007303cb2d441425a859acedea47c44e
김재석 <jaeseki@gmail.com>
2023-09-15 16:48:45 +0900

    :sparkles: Feat: #S09P22A604-101 Footer 위젯 유지시키기 - Navigator.pushReplacement:     - stack 다 지우려고 한 건데 footer로 페이지 이동 후 핸드폰 자체 뒤로가기 하면 앱이 종료된다.     - 추후 문제가 생겨서 원래대로 돌리려면 push로 변경하면 된다. - currentIndex는 홈, 코스, 등록, 캡슐, MY 순서대로 0, 1, 2, 3, 4이다. - 위 다섯(footer에 포함되는) 페이지를 제외한 페이지의 currendIndex는 해당 위젯의 최상단 부모위젯을 고려해서 설정한다. - PageRouteBuilder: 페이지 이동 시 애니메이션 삭제 - FooterWidget이 필요한 위젯에 모두 넣어주기 - appbar, drawer 형식에 맞게 변경해서 모두 추가 - initial_widget 파일 삭제: 더 이상 필요 없음

--- frontend/lib/src/screens/capsule/capsule_screen.dart
+++ frontend/lib/src/screens/capsule/capsule_screen.dart
@@ -1,37 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:youkids/src/screens/capsule/capsule_detail_screen.dart';
+import 'package:youkids/src/screens/home/home_screen.dart';
+import 'package:youkids/src/widgets/footer_widget.dart';
 
 class CapsuleScreen extends StatelessWidget {
   const CapsuleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const CapsuleDetailScreen(
-                    place: 'place',
-                    date: 'date',
-                    imgUrl: 'imgUrl',
-                    content: 'content',
-                  ),
-                ),
-              );
-            },
-            child: const Text(
-              '더보기',
-              style: TextStyle(
-                color: Color(0xffFF7E76),
-                fontSize: 16,
-              ),
-            ),
+    return Scaffold(
+      drawer: const Drawer(),
+      appBar: AppBar(
+        title: const Text(
+          'YouKids',
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('lib/src/assets/icons/bell_white.svg',
+                height: 24),
           ),
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CapsuleDetailScreen(
+                      place: 'place',
+                      date: 'date',
+                      imgUrl: 'imgUrl',
+                      content: 'content',
+                    ),
+                  ),
+                );
+              },
+              child: const Text(
+                '더보기',
+                style: TextStyle(
+                  color: Color(0xffFF7E76),
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              },
+              child: const Text('home'),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: const FooterWidget(
+        currentIndex: 3,
       ),
     );
   }
--- frontend/lib/src/screens/mypage/mypage_screen.dart
+++ frontend/lib/src/screens/mypage/mypage_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:youkids/src/widgets/footer_widget.dart';
 
 class MyPageScreen extends StatelessWidget {
   const MyPageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Text('my page'),
+    return Scaffold(
+      drawer: const Drawer(),
+      appBar: AppBar(
+        title: const Text(
+          'YouKids',
+          style: TextStyle(
+            fontSize: 22,
+            color: Colors.black,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(
+          color: Colors.black,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset('lib/src/assets/icons/bell_white.svg',
+                height: 24),
+          ),
+        ],
+      ),
+      body: const SingleChildScrollView(
+        child: Text('my page'),
+      ),
+      bottomNavigationBar: const FooterWidget(
+        currentIndex: 4,
+      ),
     );
   }
 }

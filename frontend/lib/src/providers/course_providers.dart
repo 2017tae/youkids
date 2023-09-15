b0d393af84c63acad08ed15ec6c0b7bb058ddabd
태준모 <sunjm135@naver.com>
2023-09-15 17:11:32 +0900

    :sparkles: Feat: #S09P22A604-106 코스 목록 렌더링
    - 코스 화면 유저 코스 불러오기

---
+++ frontend/lib/src/providers/course_providers.dart
@@ -0,0 +1,23 @@
+import 'package:youkids/src/models/course_models/course_detail_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class CourseProviders{
+  Uri uri = Uri.parse('');
+
+  Future<List<Course_detail_model>> getCourse() async {
+    List<Course_detail_model> courses = [];
+
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      courses = jsonDecode(utf8.decode(response.bodyBytes))['result'].map<Course_detail_model>( (result) {
+        return Course_detail_model.fromMap(result);
+      }).toList();
+    }
+
+    return courses;
+  }
+
+
+}

eea4c3966d510a0986a2dc8f1dff18f184567609
yaonggod <jy23698@naver.com>
2023-09-17 21:05:05 +0900

    :sparkles: Feat: mypage

--- frontend/lib/src/screens/mypage/mypage_screen.dart
+++ frontend/lib/src/screens/mypage/mypage_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:youkids/src/models/home_models/child_icon_model.dart';
 import 'package:youkids/src/widgets/footer_widget.dart';
 
 class MyPageScreen extends StatelessWidget {
@@ -11,7 +12,7 @@ class MyPageScreen extends StatelessWidget {
       drawer: const Drawer(),
       appBar: AppBar(
         title: const Text(
-          'YouKids',
+          '마이페이지',
           style: TextStyle(
             fontSize: 22,
             color: Colors.black,
@@ -30,8 +31,89 @@ class MyPageScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const SingleChildScrollView(
-        child: Text('my page'),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(
+                            Icons.settings,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(7),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black12),
+                              borderRadius: BorderRadius.circular(500),
+                            ),
+                            child: CircleAvatar(
+                              radius: 40,
+                              backgroundImage:
+                                  AssetImage(tmpChildStoryIcon[0].imgUrl),
+                              backgroundColor: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          const Text("은우 엄마", style: TextStyle(fontSize: 25)),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor:
+                                    const Color.fromRGBO(242, 230, 230, 1),
+                                foregroundColor: Colors.black,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5))),
+                            child: const Text("프로필 수정"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(color: Colors.black26),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: const Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "우리 아이 ",
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            size: 18,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )),
       ),
       bottomNavigationBar: const FooterWidget(
         currentIndex: 4,

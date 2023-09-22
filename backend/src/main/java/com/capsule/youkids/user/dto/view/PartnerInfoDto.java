f36098d3e1253d906dfe79eb3a8aa5fa0a8581d1
이제원 <ljw42871@gmail.com>
2023-09-22 10:07:15 +0900

    :sparkles: Feat: 유저 정보에 파트너 정보 추가
    - 유저 정보에 파트너 정보 추가

---
+++ backend/src/main/java/com/capsule/youkids/user/dto/view/PartnerInfoDto.java
@@ -0,0 +1,23 @@
+package com.capsule.youkids.user.dto.view;
+
+import com.capsule.youkids.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PartnerInfoDto {
+
+    private String email;
+
+    private String nickname;
+
+    private String profileImage;
+
+    public PartnerInfoDto(User user) {
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+        this.profileImage = user.getProfileImage();
+    }
+
+}

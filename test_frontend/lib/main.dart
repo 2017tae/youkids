ecd38ebc8006aee4d61a59579e135b66178e6565
태준모 <sunjm135@naver.com>
2023-09-26 17:53:43 +0900

    :sparkles: Feat: #S09P22A604-153 Splash Screen 구현
    - splash screen 구현 및 이미지 추가

--- test_frontend/lib/main.dart
+++ test_frontend/lib/main.dart
@@ -1,15 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:kakao_flutter_sdk_navi/kakao_flutter_sdk_navi.dart';
 import 'package:youkids/src/screens/home/home_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load(fileName: ".env");
   await NaverMapSdk.instance.initialize(clientId: dotenv.get("naver_map_key"));
   KakaoSdk.init(nativeAppKey: dotenv.get("kakao_key"));
-
+  Timer(Duration(seconds: 3), () =>
+      FlutterNativeSplash.remove());
   runApp(const YouKids());
 }
 
